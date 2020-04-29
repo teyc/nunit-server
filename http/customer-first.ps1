c9a9f0f9a383dfd58e89a96ef883a4b14cfd594c
Chui Tey <teyc@cognoware.com>
2020-04-29 17:31:52 +1000

    Run if POST

--- http/customer-first.ps1
+++ http/customer-first.ps1
@@ -4,24 +4,29 @@ $testList = "C:\dev\oss\nunit\bin\Release\net46\nunit.framework.tests.dll"
 $explore = "$HOMEDIRECTORY\$currentSession.txt"
 $result =  "$HOMEDIRECTORY\$currentSession.xml"
 $logfile = "$HOMEDIRECTORY\$currentSession.log"
+$method = $Context.Request.HttpMethod
 
 # Write-Output "$currentSession $runner $testList $explore $result $logFile"
 
-If ((Test-Path $logfile) -or ($Context.Request.Method -eq 'GET'))
+If ((Test-Path $logfile) -and ($method -eq 'GET'))
 {
     "<pre>"
     Get-Content $logfile -raw
     "</pre><script>window.scrollTo(0,document.body.scrollHeight);</script>"
 }
-else
+elseif (-not (Test-Path $logfile) -and ($method -eq 'POST'))
 {
 
     Start-Job -ScriptBlock {
-        Param ($runner, $testlist, $logfile)
-        & "$runner" "$testList" "--result=$result" >> $logfile
+        Param ($runner, $testlist, $logfile, $result)
+        & "$runner" "$testList"  >> $logfile
         Move-Item $logfile $logfile.".old" -Force
-    } -ArgumentList $runner,$testList,$logfile
+    } -ArgumentList $runner,$testList,$logfile,$result
 
     #& "$runner" "$testList" "--explore" > $explore
     $Context.Response.Redirect("./Customer-First.ps1")
+}
+else
+{
+    "Cannot start $method another test run because $logfile is present"
 }
--- http/index.ps1
+++ http/index.ps1
@@ -4,11 +4,14 @@ $currentSession = "customer-first"
 
 if ("IsRunning" -eq (Get-TestSession $currentSession))
 {
-    $status = "Status: Not Running"
+    $status = "Status: <a href=`"$currentSession.ps1`">Running</a>"
 }
 else
 {
-    $status = "<form class=`"inline`" method=`"POST`" action=`"./$currentSession.ps1`"><input type=submit value=`"Rerun all tests in current session`"></form>"
+    $status = @"
+        <form class=`"inline`" action=`"./$currentSession.ps1`"><input type=`"submit`" name=`"failed-only`" value=`"Re-run failed tests`" /></form>
+        <form class=`"inline`" method=`"POST`" action=`"./$currentSession.ps1`"><input type=submit value=`"Rerun all tests in current session`"></form>
+"@
 }
 
 $body = @"
@@ -17,8 +20,7 @@ $body = @"
 <p class="subtitle">$((Get-Item $HomeDirectory\$currentSession).FullName)</p>
 <p>Current session #15: <a href="/$currentSession.ps1">107 tests</a> = 101 passed + 3 ignored + 3 failed</p>
 <p>
-    <form class="inline" action="./new-session.ps1"><input type="submit" value="Re-run failed tests" /></form>
-    $status
+$status
 </p>
 <p><form action="./new-session.ps1"><input type="submit" value="Start new session" /></form></p>
 <div>
