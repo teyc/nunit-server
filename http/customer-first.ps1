43928935fd03e351f88240614bafdb79dee90469
Chui Tey <teyc@cognoware.com>
2020-04-29 18:44:51 +1000

    refactor  Start-TestSession

--- http/customer-first.ps1
+++ http/customer-first.ps1
@@ -1,8 +1,8 @@
+Import-Module "$PSScriptRoot\runner.psm1"
+
 $currentSession = "customer-first"
-$runner = "C:\Users\Chui\.nuget\packages\nunit.consolerunner\3.11.1\tools\nunit3-console.exe"
 $testList = "C:\dev\oss\nunit\bin\Release\net46\nunit.framework.tests.dll"
-$explore = "$HOMEDIRECTORY\$currentSession.txt"
-$result =  "$HOMEDIRECTORY\$currentSession.xml"
+
 $logfile = "$HOMEDIRECTORY\$currentSession.log"
 $method = $Context.Request.HttpMethod
 
@@ -14,23 +14,15 @@ If ((Test-Path $logfile) -and ($method -eq 'GET'))
 }
 elseif (-not (Test-Path $logfile) -and ($method -eq 'POST'))
 {
-
-    Start-Job -ScriptBlock {
-        Param ($runner, $testlist, $logfile, $result)
-        & "$runner" "$testList" "--result=$result"  >> $logfile
-
-        Start-Sleep -Seconds 5
-
-        Move-Item $logfile "$logfile.old" -Force
-    } -ArgumentList $runner,$testList,$logfile,$result
-
-    & "$runner" "$testList" "--explore" > $explore
-    Start-Sleep 5
-    # "<script>window.location = '$currentSession.ps1'</script>"
+    Start-TestSession $currentSession $HomeDirectory $testlist
     $Context.Response.Redirect("./$currentSession.ps1")
     $Context.Response.Close()
 }
-else
+elseif (-not (Test-Path $logfile) -and ($method -eq 'GET'))
+{
+    "Not found $logfile"
+}
+else # ((Test-Path $logfile) -and ($method -eq 'POST'))
 {
     "Cannot start $method another test run because $logfile is present"
 }
--- http/runner.psm1
+++ http/runner.psm1
@@ -1,3 +1,25 @@
+$RUNNER = "C:\Users\Chui\.nuget\packages\nunit.consolerunner\3.11.1\tools\nunit3-console.exe"
+
+function Start-TestSession($sessionName, $HomeDirectory, $testlist)
+{
+
+    $explore = "$HOMEDIRECTORY\$sessionName.txt"
+    $result =  "$HOMEDIRECTORY\$sessionName.xml"
+    $logfile = "$HOMEDIRECTORY\$sessionName.log"
+
+    Start-Job -ScriptBlock {
+        Param ($runner, $testlist, $logfile, $result)
+        & "$runner" "$testList" "--result=$result"  >> $logfile
+
+        Start-Sleep -Seconds 5
+
+        Move-Item $logfile "$logfile.old" -Force
+    } -ArgumentList $RUNNER,$testList,$logfile,$result
+
+    & "$RUNNER" "$testList" "--explore" > $explore
+    Start-Sleep 5
+}
+
 function Get-TestSession($sessionName)
 {
     if (Test-Path (Get-TestSessionLogPath $sessionName))
@@ -20,18 +42,18 @@ function Get-TestSessionResult($homedirectory, $sessionName)
     $file = "$HomeDirectory\$sessionName.xml"
     If (Test-Path $file)
     {
-    $xml = [xml] (Get-Content $file)
+        $xml = [xml] (Get-Content $file)
 
-    $xml.SelectNodes("//test-case") |
-        ForEach-Object {
-            [pscustomobject] @{
-                Name = $_.name
-                Duration = $_.duration
-                Result = $_.result
-                EndTime = $_."end-time"
-            }
+        $xml.SelectNodes("//test-case") |
+            ForEach-Object {
+                [pscustomobject] @{
+                    Name = $_.name
+                    Duration = $_.duration
+                    Result = $_.result
+                    EndTime = $_."end-time"
+                }
         }
-}
+    }
     else
     {
         return @()
